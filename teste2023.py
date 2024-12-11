3c7cf32e97517ad4eaf046119dcf1311b914e23a
Ribeiro <101742679+KeaplePT@users.noreply.github.com>
2024-12-11 21:11:49 +0000

    Add files via upload

---
+++ teste2023.py
@@ -0,0 +1,80 @@
+'''
+a = [1, 4, 2, 4, 3]
+r = 0 
+
+for i in a:
+    r += i * 2
+print(r)
+
+#2 10 14 22 28
+'''
+'''
+t = "hello world"
+v = "aeiou"
+r = ""
+
+for l in t:
+    if l not in v:
+        r += t
+print(r)
+
+# r = hll wrld
+'''
+'''
+i = 10
+while True:
+    if i % 2 == 0:
+        break
+    i -= 1
+print(i)
+
+# 10 
+'''
+'''
+x = "abcd"
+
+for i in x:
+    i = i.upper()
+    print(i)
+
+# A
+# B
+# C
+# D
+
+'''
+'''
+x = "abcd"
+for i in range(len(x)):
+    print(i)
+
+# 0
+# 1
+# 2
+# 3
+'''
+'''
+idade = input("por favor insira a sua idade")
+print(type(idade))
+
+class str
+'''
+
+# fazer este depois
+'''
+def resolve (list):
+    var1 = list [0]
+    var2= list [0]
+    for el in list:
+        if el > var1:
+            var1 = el
+        if el < var2:
+            var2 = el
+    return var1 - var2
+
+list = (102, 1, 23, 134, 124, 1200, 123, 12, 500, 12000)
+print(resolve(list))
+
+'''
+
+
